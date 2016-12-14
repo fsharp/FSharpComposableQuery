9da34213b66a91935d1903cb044343ffe78c4e82
Jared Hester <jared.hester@gmail.com>
2016-12-14 10:24:57 -0500

    reorganize xml tests

--- tests/FSharpComposableQuery.Tests/Xml.fs
+++ tests/FSharpComposableQuery.Tests/Xml.fs
@@ -1,13 +1,11 @@
 ﻿namespace FSharpComposableQuery.Tests
 
-open Microsoft.FSharp.Data.TypeProviders
-open Microsoft.FSharp.Quotations
-open NUnit.Framework
+open System.IO
 open System.Linq
 open System.Xml.Linq
 open FSharpComposableQuery
-open System.Data.SQLite
 open FSharp.Data.Sql
+open NUnit.Framework
 
 /// <summary>
 /// Contains example queries and operations on the Xml database. 
@@ -15,12 +13,6 @@ open FSharp.Data.Sql
 /// <para> These tests require the schema from sql/xml.sql in a database referred to in app.config </para>
 /// </summary>
 module Xml =
-    [<Literal>]
-    let xmlPath = "data\\movies.xml"
-    [<Literal>]
-    let dbConfigPath = "db.config"
-
-    let basicXml = XElement.Parse "<a id='1'><b><c>foo</c></b><d><e/><f/></d></a>"
 
     let [<Literal>] dbpath = __SOURCE_DIRECTORY__ + @"/../databases/xml.db"
     let [<Literal>] connectionString = "DataSource=" + dbpath + ";Version=3;foreign keys = true"
@@ -32,11 +24,9 @@ module Xml =
             ,   ResolutionPath = resolutionPath
             ,   CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL
             >
-    type internal DataEntity = sql.dataContext.``main.DataEntity``
-
-    type internal Text = sql.dataContext.``main.DataEntity``
-
-    type internal Attribute =  sql.dataContext.``main.AttributeEntity``
+    type DataEntity = sql.dataContext.``main.DataEntity``
+    type Text       = sql.dataContext.``main.DataEntity``
+    type Attribute  =  sql.dataContext.``main.AttributeEntity``
 
     type Axis =
         | Self
@@ -48,9 +38,9 @@ module Xml =
         | Rev of Axis
         
     type Path =
-        | Seq of Path * Path
-        | Axis of Axis
-        | Name of string
+        | Seq    of Path * Path
+        | Axis   of Axis
+        | Name   of string
         | Filter of Path
 
         static member Child = Axis Child
@@ -108,40 +98,42 @@ module Xml =
             a.Element <- parent
             a.Name <- att.Name.LocalName
             a.Value <- att.Value
+            context.SubmitUpdates()
 
         // recursively traverse all child nodes
         let traverseChildren entry parent i (xmls) = 
             Seq.fold (traverseXml entry parent) i xmls
-        let result =
-            match node with
-            | :? XElement as xml ->
-                let id = new_id()
-                let j = Seq.iter (traverseAttribute entry id) (xml.Attributes())
-                let j = traverseChildren entry id (i + 1) (xml.Nodes())
-                let d = db.Data.Create()
-                d.Name <- xml.Name.ToString()
-                d.Id <- id
-                d.Entry <- entry
-                d.Pre <- i
-                d.Post <- j
-                d.Parent <- parent
-                j + 1
-            | :? XText as xtext ->
-                let id = new_id()
-                let d = db.Data.Create()
-                d.Name <- "#text"
-                d.Id <- id
-                d.Entry <- entry
-                d.Pre <- i
-                d.Post <- i
-                d.Parent <- parent
-                let t = db.Text.Create()
-                t.Id <- id
-                t.Value <- xtext.Value
-                i + 1
-            | _ -> i
-        context.SubmitUpdates()
-        result
+        
+        match node with
+        | :? XElement as xml ->
+            let id = new_id()
+            let j = Seq.iter (traverseAttribute entry id) (xml.Attributes())
+            let j = traverseChildren entry id (i + 1) (xml.Nodes())
+            let d = db.Data.Create()
+            d.Name <- xml.Name.ToString()
+            d.Id <- id
+            d.Entry <- entry
+            d.Pre <- i
+            d.Post <- j
+            d.Parent <- parent
+            context.SubmitUpdates()
+            j + 1
+        | :? XText as xtext ->
+            let id = new_id()
+            let d = db.Data.Create()
+            d.Name <- "#text"
+            d.Id <- id
+            d.Entry <- entry
+            d.Pre <- i
+            d.Post <- i
+            d.Parent <- parent
+            let t = db.Text.Create()
+            t.Id <- id
+            t.Value <- xtext.Value
+            context.SubmitUpdates()
+            i + 1
+        | _ -> i
+        
 
     /// <summary>
     /// Parses the given file as an Xml document, and then inserts its contents in the database. 
@@ -162,33 +154,22 @@ module Xml =
 //        data.InsertOnSubmit(d)
 //        data.Context.SubmitChanges()
 
-    /// <summary>
-    /// Parses the given file as an Xml document, and then inserts its contents in the database. 
-    /// </summary>
-    /// <param name="filename">The path to the Xml file to be loaded. </param>
-    let loadXml entry (filename : string) =
-        let xml = XElement.Load(filename)
-        insertXml entry xml
 
-    /// <summary>
-    /// Loads the basicXml file
-    /// </summary>
-    let loadBasicXml() = insertXml 0 basicXml
 
     /// <summary>
     /// Returns an expression testing whether two rows match the specified axis predicate. 
     /// </summary>
     /// <param name="axis">The axis predicate to test the rows against. </param>
-    let internal axisPred axis =
+    let axisPred axis =
         let rec axisPredRec axis =
             match axis with
-            | Self -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Id = row2.Id @>
-            | Child -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Id = row2.Parent @>
-            | Descendant -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Pre < row2.Pre && row2.Post < row1.Post @>
+            | Self             -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Id = row2.Id @>
+            | Child            -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Id = row2.Parent @>
+            | Descendant       -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Pre < row2.Pre && row2.Post < row1.Post @>
             | DescendantOrSelf -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Pre <= row2.Pre && row2.Post <= row1.Post @>
-            | Following -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Post < row2.Pre @>
+            | Following        -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Post < row2.Pre @>
             | FollowingSibling -> <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Post < row2.Pre && row1.Parent = row2.Parent @>
-            | Rev axis -> <@ fun row1 row2 -> (%axisPredRec axis) row2 row1 @>
+            | Rev axis         -> <@ fun row1 row2 -> (%axisPredRec axis) row2 row1 @>
         <@ fun (row1 : DataEntity) (row2 : DataEntity) -> row1.Entry = row2.Entry && (%(axisPredRec axis)) row1 row2 @>
 
     /// <summary>
@@ -196,7 +177,7 @@ module Xml =
     /// </summary>
     /// <param name="data">The data source. </param>
     /// <param name="path">The path to run on the data source. </param>
-    let internal pathQuery data (path) =
+    let pathQuery data (path) =
         let rec pathQ path =
             match path with
             | Seq(p1, p2) ->
@@ -209,15 +190,16 @@ module Xml =
             | Name name -> <@ fun (row : DataEntity) (row' : DataEntity) -> row.Name = name && row.Id = row'.Id @>
             | Filter p ->
                 <@ fun (row : DataEntity) (row' : DataEntity) ->
-                    row.Id = row'.Id && query {
-                                            for row'' in %data do
-                                                exists ((%(pathQ p)) row row'')
-                                        } @>
-        <@ fun row1 ->
-            query {
-                for row2 in %data do
-                    if (%pathQ path) row1 row2 then yield row2
-            } @>
+                    row.Id = row'.Id 
+                    && query {
+                        for row'' in %data do
+                            exists ((%(pathQ p)) row row'')
+                } @>
+
+        <@ fun row1 -> query {
+            for row2 in %data do
+                if (%pathQ path) row1 row2 then yield row2
+        } @>
 
     
     /// <summary>
@@ -226,7 +208,7 @@ module Xml =
     /// <param name="rootId">The id of the root node. </param>
     /// <param name="data">The XML document to run the query on. </param>
     /// <param name="p">The path to construct the query from. </param>
-    let internal xpath rootId data p =
+    let xpath rootId data p =
         <@ query {
                for root in %data do
                    for row' in (%(pathQuery data p)) root do
@@ -238,6 +220,20 @@ module Xml =
     let xp2 = Path.Descendant / (Filter(Path.FollowingSibling % "dirn"))                    //  //*[following-sibling::d]
     let xp3 = Path.Descendant % "year" / Filter(Path.Ancestor / Path.Preceding % "dir")     //  //f[ancestor::*/preceding::b]
 
+    let [<Literal>] xmlPath = __SOURCE_DIRECTORY__ + "/data/movies.xml"
+
+    /// <summary>
+    /// Parses the given file as an Xml document, and then inserts its contents in the database. 
+    /// </summary>
+    /// <param name="filename">The path to the Xml file to be loaded. </param>
+    let loadXml entry (filename : string) =
+        let xml = XElement.Load(filename)
+        insertXml entry xml
+
+    let basicXml = XElement.Parse "<a id='1'><b><c>foo</c></b><d><e/><f/></d></a>"
+
+    /// Loads the basicXml file
+    let loadBasicXml() = insertXml 0 basicXml
 
     [<OneTimeSetUp>]
     let init() =
@@ -247,16 +243,16 @@ module Xml =
 
     [<Test>]
     let test01() =
-        xp0 |> xpath 0 <@ data @>  |> Utils.Run
+        xp0 |> xpath 0 <@ data.AsQueryable() @>  |> Utils.Run
 
     [<Test>]
     let test02() =
-        xp1 |> xpath 0 <@ data @> |> Utils.Run
+        xp1 |> xpath 0 <@ data.AsQueryable() @> |> Utils.Run
 
     [<Test>]
     let test03() =
-        xp2 |> xpath 0 <@ data @> |> Utils.Run
+        xp2 |> xpath 0 <@ data.AsQueryable() @> |> Utils.Run
 
     [<Test>]
     let test04() =
-        xp3 |> xpath 0 <@ data @> |> Utils.Run
+        xp3 |> xpath 0 <@ data.AsQueryable() @> |> Utils.Run
